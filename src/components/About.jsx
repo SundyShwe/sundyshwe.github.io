d3d28d19dcc6ed2948b945961e977d04902e7029
SundyShwe <sundyshwe@gmail.com>
2025-05-21 15:22:16 -0500

    About me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,13 +23,14 @@ function About() {
             <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
               <h3>About Me</h3>
               <p className="text-justify">
-                Hi, I’m Sundy — a Frontend Engineer specializing in Javascript/Typescript Frameworks.
-                During my extensive years of experience, I build fast, scalable, and user-focused web applications.
-                <br />
-                I specialize in developing clean, maintainable code with component-based architecture, responsive UI, and strong emphasis on performance and usability.
-                I thrive in Agile environments, collaborate closely with cross-functional teams, and consistently deliver user-centric features that drive impact.
-                <br />I’m a resourceful, self-driven team player who embraces new challenges as opportunities to grow.
-                When I’m not coding, you’ll find me exploring new tech, sketching UI ideas, or hunting down the best coffee in town.
+                Hi, I’m Sundy — a Frontend Engineer specializing in JavaScript and TypeScript frameworks.
+                With several years of hands-on experience, I build fast, scalable, and user-focused web applications.
+              </p>
+              <p className="text-justify">
+                I specialize in writing clean, maintainable code using component-based architecture, crafting responsive UIs, and placing a strong emphasis on performance and usability. I thrive in Agile environments, work closely with cross-functional teams, and consistently deliver impactful, user-centric features.
+              </p>
+              <p className="text-justify">
+                A resourceful and self-driven team player, I see every challenge as an opportunity to learn and grow. When I’m not coding, you’ll find me exploring new tech, sketching UI ideas, or hunting down the best coffee in town.
               </p>
               <div className="row">
                 <div className="col-lg-6">
@@ -62,7 +63,7 @@ function About() {
             </div>
           </div>
         </div>
-      </section>
+      </section >
     </>
   );
 }
